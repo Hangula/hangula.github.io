8c4e22b632702faf81155fee2bc207ec4b5c8a58
Hangula <hangula.luk@gmail.com>
2016-03-03 11:39:17 +0200

    first commit

---
+++ resources/javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  /* Add code from the next steps here */
+});
+
+$("#copyright").click(function(){ 
+  $(this).css('color','purple'); 
+});// Your Javascript will go here!
